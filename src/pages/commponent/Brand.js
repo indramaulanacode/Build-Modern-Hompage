7198bc7a493e7797f27ce09424ee86eed0e8c5ff
indramaulanacode <indramaulana4455@gmail.com>
2024-12-12 21:56:42 +0000

    brand change

--- src/pages/commponent/Brand.js
+++ src/pages/commponent/Brand.js
@@ -2,7 +2,7 @@ import React from 'react'
 const brandsData = [
   {
     imageSrc:
-      "/Img/Brand/ApplePay.svg",
+      "/Img/Brand/SVG/Google - Long.svg",
     lightImageSrc:
       "https://cdn.tailgrids.com/2.2/assets/images/brands/graygrids-white.svg",
     altText: "graygrids",
@@ -10,21 +10,21 @@ const brandsData = [
   },
   {
     imageSrc:
-      "/Img/Brand/Mastercard.svg",
+      "/Img/Brand/SVG/Microsoft.svg",
     lightImageSrc:
       "https://cdn.tailgrids.com/2.2/assets/images/brands/lineIcons-white.svg",
     altText: "lineicons",
     link: "#",
   },
   {
-    imageSrc: "/Img/Brand/Stripe.svg",
+    imageSrc: "/Img/Brand/SVG/Netflix.svg",
     lightImageSrc:
       "https://cdn.tailgrids.com/2.2/assets/images/brands/uideck-white.svg",
     altText: "uideck",
     link: "#",
   },
   {
-    imageSrc: "/Img/Brand/Stripe.svg",
+    imageSrc: "Img/Brand/SVG/PayPal - Long.svg",
     lightImageSrc:
       "https://cdn.tailgrids.com/2.2/assets/images/brands/ayroui-white.svg",
     altText: "ayroui",
@@ -35,13 +35,13 @@ const brandsData = [
 function Brand() {
 
   return (
-    <section className="bg-white py- lg:py-[20px] dark:bg-dark">
+    <section className="bg-white py- lg:py-[5px] dark:bg-dark">
     <div className="container mx-auto py-5">
       <div className="-mx-4 flex flex-wrap">
         <div className="w-full px-4">
           <div className="flex flex-wrap items-center justify-center gap-20">
             {brandsData.map((brand, i) => (
-              <img src={brand.imageSrc} className='w-20' />
+              <img src={brand.imageSrc} className='w-32' />
             ))}
           </div>
         </div>
